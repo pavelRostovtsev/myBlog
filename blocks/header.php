df39ea32c728cd7c8d2c8ec65a3b8f30cc2153ec
Павел <59763943+pavelRostovtsev@users.noreply.github.com>
2020-02-25 16:19:50 +0700

    start

---
+++ blocks/header.php
@@ -0,0 +1,33 @@
+ <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <div class="container">
+        <img src="https://i.pinimg.com/236x/6d/9c/1c/6d9c1c2951b3c3c523b9ccfd8615cf4f.jpg" width="30" height="30" class="d-inline-block align-top mx-2" alt="">
+        <span>Блог по психологии</span>
+      
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav mr-auto">
+
+            </ul>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                     <li class="nav-item ">
+                        <a class="nav-link" href="#">Блог </a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="authorization.php">Регистрация/Авторизация</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">События</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link " href="#"  >О себе</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link " href="#"  >Контакты</a>
+                      </li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?
+	require_once 'blocks/head.php';
+	?>
+</head>
+<body>
+	<?
+	require_once 'blocks/header.php';
+	?>
+	<!-- карусель -->
+<div class="container">
+	<div class="row">
+		<h1 class="mx-auto my-3 ">Авторский блог</h1>
+	</div>
+</div>
+
+<div class="content cover-container my-4">
+	<div>
+		<div class="card mx-4" style="width: 18rem;">
+  		<img src="https://cdn.website-editor.net/0cc9dba1223c460cb846a6d67186a41b/dms3rep/multi/mobile/human-1211467_1280.png" class="card-img-top" alt="...">
+  		<div class="card-body">
+    		<h5 class="card-title">Название карточки</h5>
+    		<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    		<a href="#" class="btn btn-primary">Переход куда-нибудь</a>
+  		</div>
+		</div>	
+	</div>
+	<div>
+		<div class="card mx-5" style="width: 18rem;">
+  		<img src="https://cdn.website-editor.net/0cc9dba1223c460cb846a6d67186a41b/dms3rep/multi/mobile/human-1211467_1280.png" class="card-img-top" alt="...">
+  		<div class="card-body">
+    		<h5 class="card-title">Название карточки</h5>
+    		<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    		<a href="#" class="btn btn-primary">Переход куда-нибудь</a>
+  		</div>
+		</div>	
+	</div>
+	<div>
+		<div class="card mx-5" style="width: 18rem;">
+  		<img src="https://cdn.website-editor.net/0cc9dba1223c460cb846a6d67186a41b/dms3rep/multi/mobile/human-1211467_1280.png" class="card-img-top" alt="...">
+  		<div class="card-body">
+    		<h5 class="card-title">Название карточки</h5>
+    		<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    		<a href="#" class="btn btn-primary">Переход куда-нибудь</a>
+  		</div>
+		</div>	
+	</div>
+</div>
+</body>
+</html>
+
